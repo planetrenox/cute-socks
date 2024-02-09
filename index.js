64c3f3dd9beec3969452c8d9f505dff34bff5dd0
renox <planetrenox@protonmail.com>
2024-02-09 02:50:15 -0800

    new listen

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-export const $ = selector => document.querySelector(selector);
-export const $$ = selector => document.querySelectorAll.bind(selector);
+//Yml0Lmx5L2luY2Vzc2FudC12aWJyYXRpb24=
+
+const $ = selector => document.querySelector(selector);
+const $$ = selector => document.querySelectorAll(selector);
+
+const listen = (el, handler) => el.addEventListener('click', async e => {
+    e.preventDefault();
+    await handler(e);
+});
+
+module.exports = {$, $$, listen};
